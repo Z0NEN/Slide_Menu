b5f97a4ac561cf113b55cfe6f0b37e836c4e47bf
Z0NEN <z0nen@hotmail.com>
2015-02-02 22:54:07 +0700

    Android Navigation Drawer

---
+++ app/src/main/java/app/z0nen/slidemenu/menu1_Fragment.java
@@ -0,0 +1,21 @@
+package app.z0nen.slidemenu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Z0NEN on 10/22/2014.
+ */
+public class menu1_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu1_layout, container, false);
+        return rootview;
+    }
+}
